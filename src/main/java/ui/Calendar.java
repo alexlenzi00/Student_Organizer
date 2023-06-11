90601081dbedd6b77ca02ff0b9463eb8711e17fa
alexlenzi00 <lenzi-alex@libero.it>
2023-06-11 11:42:16 +0200

    Initial commit

--- src/main/java/ui/Calendar.java
+++ src/main/java/ui/Calendar.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -12,8 +13,6 @@ import util.Util;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.WEEKS;
@@ -23,7 +22,7 @@ public class Calendar extends ScreenUI {
 	protected LocalDate actualMonday;
 	protected LocalDate calendarMonday;
 	protected int weekOffset;
-	protected Map<LocalDate, ArrayList<Block>> blocks;
+	protected ArrayList<Block> blocks;
 	protected ArrayList<Label> days;
 
 	public Calendar() {
@@ -31,89 +30,75 @@ public class Calendar extends ScreenUI {
 		LocalDate now = LocalDate.now();
 		calendarMonday = actualMonday = now.minus(now.getDayOfWeek().getValue() - 1, DAYS);
 		weekOffset = 0;
-		blocks = new LinkedHashMap<>();
+		blocks = new ArrayList<>();
 		root = new AnchorPane();
 		root.setId("background");
 		root.setPrefHeight(860);
 		root.setPrefWidth(1600);
 		days = new ArrayList<>();
-		// colonne del calendario
-		{
-			Label colonna0 = new Label();
-			colonna0.setId("calendarCol");
-			colonna0.setPrefSize(100.0, 765.0);
-			colonna0.setLayoutX(400);
-			colonna0.setLayoutY(35);
-			root.getChildren().add(colonna0);
-			for (int i = 0; i < 7; i++) {
-				Label dayLabel = new Label();
-				dayLabel.setId("calendarCol");
-				dayLabel.setLayoutX(getLabelX(i));
-				dayLabel.setLayoutY(35);
-				dayLabel.setPrefSize(150.0, 765.0);
-				dayLabel.setTextAlignment(TextAlignment.CENTER);
-				dayLabel.setAlignment(Pos.TOP_CENTER);
-				dayLabel.setWrapText(true);
-				root.getChildren().add(dayLabel);
-				days.add(dayLabel);
-			}
+		Label colonna0 = new Label();
+		colonna0.setId("calendarCol");
+		colonna0.setPrefSize(100.0, 765.0);
+		colonna0.setLayoutX(400);
+		colonna0.setLayoutY(35);
+		root.getChildren().add(colonna0);
+		for (int i = 0; i < 7; i++) {
+			Label dayLabel = new Label();
+			dayLabel.setId("calendarCol");
+			dayLabel.setLayoutX(getLabelX(i));
+			dayLabel.setLayoutY(35);
+			dayLabel.setPrefSize(150.0, 765.0);
+			dayLabel.setTextAlignment(TextAlignment.CENTER);
+			dayLabel.setAlignment(Pos.TOP_CENTER);
+			dayLabel.setWrapText(true);
+			root.getChildren().add(dayLabel);
+			days.add(dayLabel);
 		}
-		// righe del calendario
-		{
-			Label riga0 = new Label();
-			riga0.setId("calendarRow");
-			riga0.setLayoutX(400);
-			riga0.setLayoutY(50);
-			riga0.setPrefSize(1150.0, 50.0);
-			root.getChildren().add(riga0);
-			for (int i = 7; i < 21; i++) {
-				Label hour = new Label();
-				hour.setId("calendarRow");
-				hour.setLayoutX(400);
-				hour.setLayoutY(getLabelY(i - 7));
-				hour.setPrefSize(1150.0, 50.0);
-				hour.setText(String.format("%02d:00 - %02d:00", i, i + 1));
-				root.getChildren().add(hour);
-			}
+		Label riga0 = new Label();
+		riga0.setId("calendarRow");
+		riga0.setLayoutX(400);
+		riga0.setLayoutY(50);
+		riga0.setPrefSize(1150.0, 50.0);
+		root.getChildren().add(riga0);
+		for (int i = 7; i < 21; i++) {
+			Label hour = new Label();
+			hour.setId("calendarRow");
+			hour.setLayoutX(400);
+			hour.setLayoutY(getLabelY(i - 7));
+			hour.setPrefSize(1150.0, 50.0);
+			hour.setText(String.format("%02d:00 - %02d:00", i, i + 1));
+			root.getChildren().add(hour);
 		}
-		// bottoni di controllo
 		Label lChangeWeek = new Label("Cambia\nsettimana");
-		{
-			lChangeWeek.setId("T2");
-			lChangeWeek.setPrefSize(200, 100);
-			lChangeWeek.setLayoutX(100);
-			lChangeWeek.setLayoutY(600);
-			lChangeWeek.setAlignment(Pos.CENTER);
-			lChangeWeek.setTextAlignment(TextAlignment.CENTER);
-			lChangeWeek.setWrapText(true);
-			root.getChildren().add(lChangeWeek);
-		}
+		lChangeWeek.setId("T2");
+		lChangeWeek.setPrefSize(200, 100);
+		lChangeWeek.setLayoutX(100);
+		lChangeWeek.setLayoutY(600);
+		lChangeWeek.setAlignment(Pos.CENTER);
+		lChangeWeek.setTextAlignment(TextAlignment.CENTER);
+		lChangeWeek.setWrapText(true);
+		root.getChildren().add(lChangeWeek);
 		Button btWeekPrevious = new Button("<");
-		{
-			btWeekPrevious.setPrefSize(50, 50);
-			btWeekPrevious.setLayoutX(50);
-			btWeekPrevious.setLayoutY(625);
-			btWeekPrevious.setOnAction(actionEvent -> previous());
-			root.getChildren().add(btWeekPrevious);
-		}
+		btWeekPrevious.setPrefSize(50, 50);
+		btWeekPrevious.setLayoutX(50);
+		btWeekPrevious.setLayoutY(625);
+		btWeekPrevious.setOnAction(actionEvent -> previous());
+		root.getChildren().add(btWeekPrevious);
 		Button btWeekNext = new Button(">");
-		{
-			btWeekNext.setPrefSize(50, 50);
-			btWeekNext.setLayoutX(300);
-			btWeekNext.setLayoutY(625);
-			btWeekNext.setOnAction(actionEvent -> next());
-			root.getChildren().add(btWeekNext);
-		}
+		btWeekNext.setPrefSize(50, 50);
+		btWeekNext.setLayoutX(300);
+		btWeekNext.setLayoutY(625);
+		btWeekNext.setOnAction(actionEvent -> next());
+		root.getChildren().add(btWeekNext);
 		Button btWeekReset = new Button("Settimana attuale");
-		{
-			btWeekReset.setPrefSize(300, 50);
-			btWeekReset.setLayoutX(50);
-			btWeekReset.setLayoutY(700);
-			btWeekReset.setOnAction(actionEvent -> reset());
-			root.getChildren().add(btWeekReset);
-		}
+		btWeekReset.setPrefSize(300, 50);
+		btWeekReset.setLayoutX(50);
+		btWeekReset.setLayoutY(700);
+		btWeekReset.setOnAction(actionEvent -> reset());
+		root.getChildren().add(btWeekReset);
 		update();
 		setTitle("Student Organizer - Calendar");
+		setScene(new Scene(root, root.getPrefWidth(), root.getPrefHeight()));
 	}
 
 	private double getLabelX(double col) {
@@ -127,10 +112,8 @@ public class Calendar extends ScreenUI {
 	@Override
 	protected void update() {
 		calendarMonday = actualMonday.plus(weekOffset, WEEKS);
-		for (LocalDate date : blocks.keySet()) {
-			for (Block block : blocks.get(date)) {
-				root.getChildren().remove(block);
-			}
+		for (Block block : blocks) {
+			root.getChildren().remove(block);
 		}
 		blocks.clear();
 		ArrayList<Lesson> lessons = Lesson.loadAll();
@@ -158,14 +141,12 @@ public class Calendar extends ScreenUI {
 			double x = getLabelX(calendarMonday.until(lesson.getDay()).getDays());
 			double y = getLabelY(lesson.getStartHour().getHour() - 7 + lesson.getStartHour().getMinute() / 60.0);
 			LocalTime duration = lesson.getDuration();
-			LocalDate date = lesson.getDay();
-			blocks.computeIfAbsent(date, k -> new ArrayList<>());
 			Block block = new Block(text, state, x, y, duration.toSecondOfDay() / 60.0);
 			block.setOnMouseClicked(mouseEvent -> {
 				LessonSummary lessonSummary = new LessonSummary(App.getStage(), lesson);
 				lessonSummary.show();
 			});
-			blocks.get(lesson.getDay()).add(block);
+			blocks.add(block);
 			root.getChildren().add(block);
 		}
 		for (int i = 0; i < 7; i++) {
